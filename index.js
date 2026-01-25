5c23bc1c0f19d71d9cce9ed339eb6a227f2ebdd9
Ariel Gonzalez Maillard <87766377+AMGM2@users.noreply.github.com>
2026-01-25 17:05:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import crypto from "crypto";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Variables de entorno
+const TN_APP_ID = process.env.TN_APP_ID;
+const TN_CLIENT_SECRET = process.env.TN_CLIENT_SECRET;
+const TN_STORE_ID = process.env.TN_STORE_ID;
+const TN_ACCESS_TOKEN = process.env.TN_ACCESS_TOKEN;
 /* ======================
    CONFIG
 ====================== */
